79e843eb8677df8fe696bab918cd82c5099ba516
Yasser Toruno <yasser@yassers-mbp.lan>
2026-02-18 00:39:30 -0500

    configure as plugin for dj-control-room

---
+++ dj_urls_panel/panel.py
@@ -0,0 +1,8 @@
+class UrlsPanel:
+    id = "dj_urls_panel"
+    name = "URLs Panel"
+    description = "Inspect and search your project's URL patterns"
+    icon = "link"
+
+    def get_url_name(self):
+        return "index"
